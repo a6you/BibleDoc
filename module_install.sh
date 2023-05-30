375929f75aab6143e281ec45ce82cabed32c3e7c
Austin You <austin.you@uvaro.com>
2023-05-29 22:50:12 -0400

    Started installation script for needed Python modules

---
+++ module_install.sh
@@ -0,0 +1,6 @@
+pip3 install bs4
+pip3 install requests
+pip3 install urllib3==1.26.6 # urllib3 v2.0 only supports OpenSSL 1.1.1+, urllib3 v2.0 only supports OpenSSL 1.1.1+, currently the 'ssl' module is compiled with LibreSSL 2.8.3
+pip3 install urllib3 # Upgrades urllib3 if possible
+
+pip3 install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib # Install Google API libraries
